9d9b24453599ff7606486cecfdc80b2c3587e139
Andrii Badieiev <andrey64freemail@gmail.com>
2020-03-07 21:42:28 +0200

    init

---
+++ IdentityTest2/Models/IdentityModels.cs
@@ -0,0 +1,63 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace IdentityTest2.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+
+    public class DatabaseInitializer : CreateDatabaseIfNotExists<ApplicationDbContext>
+    {
+        protected override void Seed(ApplicationDbContext context)
+        {
+            var userManager = new ApplicationUserManager(new UserStore<ApplicationUser>(context));
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>());
+
+            var adminRole = new IdentityRole { Name = "admin" };
+            var managerRole = new IdentityRole { Name = "manager" };
+            var userRole = new IdentityRole { Name = "user" };
+
+            roleManager.Create(adminRole);
+            roleManager.Create(managerRole);
+            roleManager.Create(userRole);
+
+            var admin = new ApplicationUser { Email = "admin@mail.com", UserName = "admin@mail.com" };
+            string password = "Admin_1";
+
+            var result = userManager.Create(admin, password);
+
+            if (result.Succeeded)
+            {
+                userManager.AddToRole(admin.Id, adminRole.Name);
+                userManager.AddToRole(admin.Id, managerRole.Name);
+            }
+
+            base.Seed(context);
+        }
+    }
+}
