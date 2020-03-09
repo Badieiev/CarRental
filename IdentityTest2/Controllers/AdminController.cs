9a4e18fa852d9b90ef6d7d48bc3db24e3b3c06f3
Andrii Badieiev <andrey64freemail@gmail.com>
2020-03-09 10:41:46 +0200

    Create Manager

--- IdentityTest2/Controllers/AdminController.cs
+++ IdentityTest2/Controllers/AdminController.cs
@@ -1,11 +1,13 @@
 ﻿using IdentityTest2.App_Start;
 using IdentityTest2.Models;
+using Microsoft.Ajax.Utilities;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 
@@ -59,14 +61,14 @@ namespace IdentityTest2.Controllers
             var managerRole = roleManager.FindByName("manager");
 
             if (managerRole == null)
-                return HttpNotFound();
+                throw new HttpRequestException();
 
             var managers = TheUserManager.Users
                 .Where(u => u.Roles.Select(r => r.RoleId)
                 .Contains(managerRole.Id));
             
             if (managers == null)
-                return HttpNotFound();
+                throw new HttpRequestException();
 
             return View(managers);
         }
@@ -78,16 +80,58 @@ namespace IdentityTest2.Controllers
             var manager = TheUserManager.FindById(id);
 
             if (manager == null)
-                return HttpNotFound();
+                throw new HttpRequestException();
 
-            return View();
+            return View(manager);
         }
 
         [HttpPost]
         [Authorize(Roles = "admin")]
         public ActionResult EditManager(ApplicationUser manager)
         {
-            return null;
+            if (!ModelState.IsValid) throw new HttpRequestException();
+
+            var user = TheUserManager.FindById(manager.Id);
+            if (user == null) throw new HttpRequestException();
+
+            user.Email = manager.Email;
+            user.UserName = manager.Email;
+
+            TheUserManager.Update(user);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "admin")]
+        public ActionResult CreateManager()
+        {
+            if (!ModelState.IsValid) throw new HttpRequestException();
+
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "admin")]
+        public ActionResult CreateManager(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid) throw new HttpRequestException();
+
+            var user = new ApplicationUser { UserName = model.Email, Email = model.Email };            
+            var result = TheUserManager.Create(user, model.Password);
+            if (result.Succeeded)
+            {
+                var manager = TheUserManager.FindByEmail(user.Email);
+                TheUserManager.AddToRole(manager.Id, "manager");
+                return RedirectToAction("Index", "Home");
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+
+            return View(model);
         }
     }
 }
